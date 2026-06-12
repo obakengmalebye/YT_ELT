330c69011eeb13ee718df651f8aa1a9fcaf7366b
Malebye <F5444799@frg-mba-djw33vk20n.fnb.co.za>
2026-06-12 22:00:48 +0200

    added .env to git ignore

---
+++ video_stats.py
@@ -0,0 +1,42 @@
+import json
+
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY=os.getenv("API_KEY")
+
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_Id():
+    try:
+        url = "https://youtube.googleapis.com/youtube/v3/channels"
+
+        params = {
+            "part": "contentDetails",
+            "forHandle": CHANNEL_HANDLE,
+            "key": API_KEY,
+        }
+
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+       # print(json.dumps(data, indent=4))
+
+        channel_items = data["items"][0]
+        channel_playlist_id = (
+            channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        )
+        #print(channel_playlist_id)
+        return channel_playlist_id
+
+    except requests.exceptions.RequestException as e:
+        raise e
+if __name__ == "__main__":
+
+    get_playlist_Id()
+
+
+
